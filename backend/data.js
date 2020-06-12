711abbaef0897a5eae1627e94fc246518efc45f1
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2020-06-12 14:36:51 -0400

    Created backend REST-API and use axios to fetch data in frontend

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: 1,
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      brand: 'Nike',
+      price: 60,
+      review: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: 2,
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      brand: 'Nike',
+      price: 50,
+      review: 3.5,
+      numReviews: 5
+    },
+    {
+      _id: 3,
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      brand: 'Nike',
+      price: 40,
+      review: 6.5,
+      numReviews: 8
+    }
+  ]
+}
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import data from '../data';
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function HomePage() {
+
+  const [products, setProducts] = useState([]); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    }
+    fetchData()
+    return () => {
+      //
+    }
+  }, []);
+
   return (
     <ul className="products">
     {
-      data.products.map(product =>
+     products.map(product =>
         <li key={product._id}>
           <div className="product">
             <Link to={`/products/${product._id}`}>
